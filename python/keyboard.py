c88421f0cc1ecd0916c722ee005e0f0d7de07ddd
zk <liuzq7@gmail.com>
2020-02-18 12:42:00 +0800

    add key board

--- python/keyboard.py
+++ python/keyboard.py
@@ -1,4 +1,13 @@
 from pynput import keyboard
+import os
+applescript = """
+reattach-to-user-namespace osascript -e 'display notification "{}" with title "{}"'
+"""
+
+
+def notification(title="", content=""):
+    os.system(applescript.format(content, title))
+
 
 def on_press(key):
     try:
@@ -7,17 +16,18 @@ def on_press(key):
         # print('special key {0} pressed'.format(
         #     key))
         pass
-        
+
 
 def on_release(key):
-    print(key)
+
     if key == keyboard.Key.esc:
         # Stop listener
-        return False
+        # return False
+        return True
+
 
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
-    
